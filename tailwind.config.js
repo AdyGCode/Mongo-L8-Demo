595a60c1f28b029cdc03218d79bd8ee0f4d46ce4
Adrian G (NMT) <Adrian.Gould@nmtafe.wa.edu.au>
2021-12-03 09:34:04 +0800

    Added Tailwind Extras, Breeze UI Base, FontAwesome Free Base

---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+        colors: {
+            // Build your palette here
+            transparent: 'transparent',
+            current: 'currentColor',
+            white: colors.white,
+            black: colors.black,
+            blueGray: colors.blueGray,
+            coolGray: colors.coolGray,
+            gray: colors.gray,
+            trueGray: colors.trueGray,
+            warmGray: colors.warmGray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+
+            /* Alternative names for some colours */
+            primary: colors.blue,
+            secondary: colors.purple,
+            tertiary: colors.gray,
+            danger: colors.red,
+            warning: colors.amber,
+            info: colors.blueGray,
+            success: colors.green,
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+        require('tailwindcss-gradients'),
+        require('tailwindcss-elevation')(['responsive']),
+    ],
+}
+
